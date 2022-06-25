9f26229225b08f35009ec57e3adf174a27225a14
Stefano Corra <stefano.corra@hotmail.com>
2022-06-24 23:51:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import Movie from './Movie';
 
-// const apiKey = "fec8b5ab27b292a68294261bb21b04a5";
+// const apiKey = "fec8b5ab27b292a68294261bb21a5";
 // const singleMovieUrl = "https://api.themoviedb.org/3/movie/"
 // const nowPlayingUrl = "https://api.themoviedb.org/3/movie/now_playing";
 // const imageUrl = "http://image.tmdb.org/t/p/w300";
